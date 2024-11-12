e0379e2bfbc14deb9ad79a0392c758f0a7c0767e
Pratiksha <pratu@gmail.com>
2024-11-12 18:01:00 +0530

    Array object created for entering the data

---
+++ AddUserForm.js
@@ -0,0 +1,33 @@
+let users=[];
+let form=document.querySelector("form");
+form.addEventListener("submit",function(event){
+    event.preventDefault();
+    let userdata={
+        id:document.getElementById("userId").value,
+        firstname:document.getElementById("firstname").value,
+        lastname:document.getElementById("lastname").value,
+        password:document.getElementById("password").value,
+        email:document.getElementById("email").value,
+        username:document.getElementById("username").value,
+        phone:document.getElementById("phone").value,
+        active:document.getElementById("active").value,
+    };
+    users.push(userdata);
+    // console.log(userdata);
+    form.reset();
+
+});
+function addRowToTable(){
+    let row=document.createElement("tr");
+    row.innerHTML=`<tr id="tablerow">
+                        <td class="p-2">${users.id}</td>
+                        <td class="p-2">${users.firstname}</td>
+                        <td class="p-2">${users.lastname}</td>
+                        <td class="p-2">${users.password}</td>
+                        <td class="p-2">${users.email}</td>
+                        <td class="p-2">${users.username}</td>
+                        <td class="p-2">${users.phone}</td>
+                        <td class="p-2 text-sla">${users.active}</td>
+                    </tr>`;
+    document.querySelector("#tablebody").appendChild(row);
+}
--- user.js
+++ user.js
@@ -1,76 +1,80 @@
-let users = [
-    {
-        "id": "1",
-        "firstname": "John",
-        "lastname": "Doe",
-        "password": "newpass",
-        "email": "naveen.ara@gmail.com",
-        "username": "test1",
-        "phone": "9654602735",
-        "active": 1
-    },
-    {
-        "id": "2",
-        "lastname": "another-user",
-        "password": "password123",
-        "email": "another@example.com",
-        "username": "test2",
-        "phone": "9876543210",
-        "active": 1
-    },
-    {
-        "id": "3",
-        "firstname": "test3",
-        "lastname": "third-user",
-        "password": "pass456",
-        "email": "third@example.com",
-        "username": "test3",
-        "phone": "1234567890",
-        "active": 0
-    },
-    {
-        "id": "4",
-        "firstname": "test3",
-        "lastname": "third-user",
-        "password": "pass456",
-        "email": "third@example.com",
-        "username": "test3",
-        "phone": "1234567890",
-        "active": 0
-    }
-];
+// let users = [
+//     {
+//         "id": "1",
+//         "firstname": "John",
+//         "lastname": "Doe",
+//         "password": "newpass",
+//         "email": "naveen.ara@gmail.com",
+//         "username": "test1",
+//         "phone": "9654602735",
+//         "active": 1
+//     },
+//     {
+//         "id": "2",
+//         "lastname": "another-user",
+//         "password": "password123",
+//         "email": "another@example.com",
+//         "username": "test2",
+//         "phone": "9876543210",
+//         "active": 1
+//     },
+//     {
+//         "id": "3",
+//         "firstname": "test3",
+//         "lastname": "third-user",
+//         "password": "pass456",
+//         "email": "third@example.com",
+//         "username": "test3",
+//         "phone": "1234567890",
+//         "active": 0
+//     },
+//     {
+//         "id": "4",
+//         "firstname": "test3",
+//         "lastname": "third-user",
+//         "password": "pass456",
+//         "email": "third@example.com",
+//         "username": "test3",
+//         "phone": "1234567890",
+//         "active": 0
+//     }
+// ];
 
-let addUserBtn = document.querySelector("#usertitleright button");
-let tableBody = document.querySelector("#tablebody");
+// let addUserBtn = document.querySelector("#usertitleright button");
+// let tableBody = document.querySelector("#tablebody");
 
-function addUserRow(user)
-{
-    let newRow=`<tr>
-                    <td class="p-2">${user.id}</td>
-                    <td class="p-2">${user.firstname}</td>
-                    <td class="p-2">${user.lastname}</td>
-                    <td class="p-2">${user.password}</td>
-                    <td class="p-2">${user.email}</td>
-                    <td class="p-2">${user.username}</td>
-                    <td class="p-2">${user.phone}</td>
-                    <td class="p-2">${user.active}</td>
-                </tr>`;
-        tableBody.innerHTML+=newRow;
-}
-addUserBtn.addEventListener("click",function()
-{
-    let userIndex=0;
-    if(userIndex<users.length)
-    {
-        let user=users[userIndex];
-        addUserRow(user);
+// function addUserRow(user)
+// {
+//     let newRow=`<tr>
+//                     <td class="p-2">${user.id}</td>
+//                     <td class="p-2">${user.firstname}</td>
+//                     <td class="p-2">${user.lastname}</td>
+//                     <td class="p-2">${user.password}</td>
+//                     <td class="p-2">${user.email}</td>
+//                     <td class="p-2">${user.username}</td>
+//                     <td class="p-2">${user.phone}</td>
+//                     <td class="p-2">${user.active}</td>
+//                 </tr>`;
+//         tableBody.innerHTML+=newRow;
+// }
+// addUserBtn.addEventListener("click",function()
+// {
+//     let userIndex=0;
+//     if(userIndex<users.length)
+//     {
+//         let user=users[userIndex];
+//         addUserRow(user);
 
 
-        userIndex++;
-    }
-    else
-    {
-        alert("No more users to add");
-    }
-})
+//         userIndex++;
+//     }
+//     else
+//     {
+//         alert("No more users to add");
+//     }
+// })
 
+// function addUserData()
+// {
+//     window.location.href="AddUserForm.html";
+// }
